f0f32fcdffc5865cedb10afaccbc47644dcd5a58
Archit Bhonsle <abhonsle2000@gmail.com>
2020-06-16 10:42:17 +0530

    Updates

---
+++ precache-manifest.9f06a8da8df07dd55e862b1db9d3ccd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afad7b1a835dc6228f11cb4e831d8a64",
+    "url": "/habit-tracker-react-ts/index.html"
+  },
+  {
+    "revision": "43a3a459a7bc3f5f6dd1",
+    "url": "/habit-tracker-react-ts/static/css/main.4bb51065.chunk.css"
+  },
+  {
+    "revision": "8e5c58173382fde87a06",
+    "url": "/habit-tracker-react-ts/static/js/2.2ff63d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/habit-tracker-react-ts/static/js/2.2ff63d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a3a459a7bc3f5f6dd1",
+    "url": "/habit-tracker-react-ts/static/js/main.8a76aedb.chunk.js"
+  },
+  {
+    "revision": "810e179a64109569c390",
+    "url": "/habit-tracker-react-ts/static/js/runtime-main.1b8a773b.js"
+  }
+]);
